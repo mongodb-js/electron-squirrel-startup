b778c70ff766ce94f267f9e039debb35c6bd4ec2
Jeff Bargmann <jeffba@yahoo-inc.com>
2016-08-11 23:09:33 -0700

    Add ability to explicitly set icon during --createShortcut on install

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var path = require('path');
 var spawn = require('child_process').spawn;
 var debug = require('debug')('electron-squirrel-startup');
 var app = require('electron').app;
+var fs = require('fs');
 
 var run = function(args, done) {
   var updateExe = path.resolve(path.dirname(process.execPath), '..', 'Update.exe');
@@ -11,14 +12,31 @@ var run = function(args, done) {
   }).on('close', done);
 };
 
+var copyIcon = function(sourceFile) {
+  var output, targetFile;
+  try {
+    targetFile = path.resolve(path.dirname(process.execPath), '..', 'app.ico');
+    fs.writeFileSync(targetFile, fs.readFileSync(sourceFile));
+    output = targetFile;
+  } catch(err) {
+    debug('Failed to copy icon `%s` to `%s` %s', sourceFile, targetFile, err.message);
+  }
+  return output;
+};
+
 var check = function(options) {
   if (process.platform === 'win32') {
-    var cmd = process.argv[1];
+    var cmd = process.argv[1], args;
     debug('processing squirrel command `%s`', cmd);
     var target = path.basename(process.execPath);
 
     if (cmd === '--squirrel-install' || cmd === '--squirrel-updated') {
-      run(['--createShortcut=' + target + ''], app.quit);
+      args = ['--createShortcut=' + target + ''];
+      
+      var iconPath = options && options.iconPath && copyIcon(options.iconPath);
+      if(iconPath) args.push('--i=' + iconPath);
+
+      run(args, app.quit);
       return true;
     }
     if (cmd === '--squirrel-uninstall') {
